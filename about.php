4286e2ba0593655c4789ae38619f4a937dcaba9b
BESTHA-JAYAPRAKASH888 <104303284+BESTHA-JAYAPRAKASH888@users.noreply.github.com>
2022-11-01 23:20:34 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#E2502C">Online Student Feedback Form For Teaching Faculty</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Student Feedback form for teaching faculty in a College 
+Here we will develop the a faculty's based feedback system, which is generally used in the college to rate and analysis of the faculty's performance...Here we have 3 modules such as administrator, student,faculty.
+
+as Administrator is the one who creates the student account and by adding all faculties information and assigning the username and password. 
+Admin als0 checks the result once all students entered the feedback..
+We can start the development from the login page, where we have given the option to login as admin and student.
+
+You can perform all admin actions such as login to the account and check result..
+student before login into potral than he can register with their details
+Ifyou entered the student user and password and selected student option, then it will show all student information and let you enter the feedback based on the subject..
+
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
